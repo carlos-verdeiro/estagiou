5b35de15536e260e795a6df91fd2de49aa5463e8
CarlosDanielVerdeiro <carlos.d.verdeiro@gmail.com>
2024-03-02 14:53:49 -0300

    adicionando título

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                     <span class="navbar-toggler-icon"></span>
                     MENU
                 </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -54,18 +54,24 @@
                         </li>
                     </ul>
                     <!--BOTÕES CADASTRO E LOGIN-->
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <button type="button" class="btn btn-success" aria-current="page">Realizar Cadastro</button>
-                        </li>
+                    <ul class="navbar-nav mb-2 mb-lg-0 w-25 column-gap-3">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Entrar</a>
+                        </li>
+                        <li class="nav-item center-h">
+                            <button type="button" class="btn btn-success" aria-current="page">Realizar Cadastro</button>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
+
+    <section class="container-fluid" id="sectionBanner">
+        <img src="assets/img/logo/logo.svg" alt="Banner da empresa" id="logoSVG">
+        <img src="assets/img/logo/titulo.svg" alt="Título da empresa" id="tituloSVG">
+
+    </section>
 </body>
 
 </html>
