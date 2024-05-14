1ee1052a59f88a7e06f57597570e2f2ee41c4958
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-05-14 15:02:22 -0300

    alteração cadastro

---
+++ public/cadastro/estagiario/action.php
@@ -0,0 +1,17 @@
+<script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/ring.js"></script>
+
+<l-ring
+  size="40"
+  stroke="5"
+  bg-opacity="0"
+  speed="2"
+  color="black" 
+></l-ring>
+
+<?php
+
+if ($_SESSION['']) {
+    # code...
+}
+
+?>
--- public/templates/cadastro/selecao.php
+++ public/templates/cadastro/selecao.php
@@ -1,8 +1,8 @@
 <div class="divInputs d-flex flex-wrap w-100">
     <h2 class="h2">Se inscrever como</h2>
     <form action="cadastro.php" method="post">
-        <a href="estagiario/etapa1.php" class="btn btn-dark botoesCadastro" id="cadastroEstagiario">Estagiário</a>
-        <a href="ie/etapa1.php" class="btn btn-warning botoesCadastro" id="cadastroIE">Instituição de ensino</a>
-        <a href="empresa/etapa1.php" class="btn btn-info botoesCadastro" id="cadastroEmpresa">Empresa</a>
+        <a href="estagiario/action.php" class="btn btn-dark botoesCadastro" id="cadastroEstagiario">Estagiário</a>
+        <a href="ie/action.php" class="btn btn-warning botoesCadastro" id="cadastroIE">Instituição de ensino</a>
+        <a href="empresa/action.php" class="btn btn-info botoesCadastro" id="cadastroEmpresa">Empresa</a>
     </form>
 </div>
