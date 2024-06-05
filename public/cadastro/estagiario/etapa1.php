6e5ab2b3e87b30bae2beab90e1558809e43c85e3
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-06-05 15:21:46 -0300

    form1
    finalização modelagem, falta validar com jquery todos os campos e bootstrap

--- public/cadastro/estagiario/etapa1.php
+++ public/cadastro/estagiario/etapa1.php
@@ -7,7 +7,6 @@
     <title>Etapa 1</title>
 
     <link rel="stylesheet" href="../../../assets/css/cadastro/etapas.css">
-    <script src="../../../assets/js/cadastro/validacao1.js"></script>
 
     <!--BIBLIOTECAS-->
 
@@ -20,6 +19,9 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js"
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script><!--PLUGIN JQUERY-->
+
     <!--FIM JQUERY-->
 
     <!--FIM BIBLIOTECAS-->
@@ -75,6 +77,8 @@
             </div>
         </form>
     </section>
+    <script src="../../../assets/js/cadastro/validacao1.js"></script>
+
 </body>
 
 </html>
