8b4d45858f373cef50cf2e7caef9d130431cd2e2
Henrique Ferreira Pires <henrique.fpires10@gmail.com>
2024-07-14 17:26:10 -0300

    pagina-sobre

---
+++ public/pages/sobre.php
@@ -0,0 +1,9 @@
+<section id="sectionSobre">
+    <link rel="stylesheet" href="assets/css/index/sobre.css">
+
+   <div class="div1"> 
+    <h1>Algo sobre nós</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+
+    </div>
+</section>
