e006177b115482eda271299bfa0b4f5378868ec1
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-07-15 04:39:20 -0300

    atualização DB

--- estagiou.sql
+++ estagiou.sql
@@ -1,27 +1,99 @@
-CREATE TABLE `escola`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` VARCHAR(255) NOT NULL,
-    `telefone` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
-    `cnpj` BIGINT NOT NULL
-);
-ALTER TABLE
-    `escola` ADD UNIQUE `escola_email_unique`(`email`);
-ALTER TABLE
-    `escola` ADD UNIQUE `escola_cnpj_unique`(`cnpj`);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 15/07/2024 às 09:38
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
-CREATE TABLE `empresa`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nome` VARCHAR(255) NOT NULL,
-    `telefone` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `cnpj` BIGINT NOT NULL
-);
-ALTER TABLE
-    `empresa` ADD UNIQUE `empresa_email_unique`(`email`);
-ALTER TABLE
-    `empresa` ADD UNIQUE `empresa_cnpj_unique`(`cnpj`);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `estagiou`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cnpj` varchar(20) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `numero` varchar(50) NOT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `estado` varchar(5) NOT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `pais` varchar(100) NOT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `nome_responsavel` varchar(255) NOT NULL,
+  `cargo_responsavel` varchar(100) NOT NULL,
+  `telefone_responsavel` varchar(25) NOT NULL,
+  `email_responsavel` varchar(255) NOT NULL,
+  `area_atuacao` varchar(100) NOT NULL,
+  `descricao` varchar(500) NOT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `linkedin` varchar(100) DEFAULT NULL,
+  `instagram` varchar(100) DEFAULT NULL,
+  `facebook` varchar(100) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `escola`
+--
+
+CREATE TABLE `escola` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cnpj` varchar(20) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `numero` varchar(50) NOT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `estado` varchar(5) NOT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `pais` varchar(100) NOT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `nome_responsavel` varchar(255) NOT NULL,
+  `cargo_responsavel` varchar(100) NOT NULL,
+  `telefone_responsavel` varchar(25) NOT NULL,
+  `email_responsavel` varchar(255) NOT NULL,
+  `niveis_ensino` varchar(100) NOT NULL,
+  `descricao` varchar(500) NOT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `linkedin` varchar(100) DEFAULT NULL,
+  `instagram` varchar(100) DEFAULT NULL,
+  `facebook` varchar(100) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `estagiario`
+--
 
 CREATE TABLE `estagiario` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -30,7 +102,7 @@ CREATE TABLE `estagiario` (
   `nome` varchar(100) NOT NULL,
   `sobrenome` varchar(100) DEFAULT NULL,
   `estado_civil` varchar(50) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
   `rg` varchar(20) NOT NULL,
   `rg_org_emissor` varchar(50) NOT NULL,
   `data_nascimento` date NOT NULL,
@@ -46,51 +118,50 @@ CREATE TABLE `estagiario` (
   `genero` varchar(50) NOT NULL,
   `nome_social` varchar(255) DEFAULT NULL,
   `endereco` varchar(255) NOT NULL,
-  `numero` int(11) NOT NULL,
+  `numero` varchar(50) NOT NULL,
   `complemento` varchar(255) DEFAULT NULL,
   `cidade` varchar(100) NOT NULL,
   `estado` varchar(100) NOT NULL,
   `cep` varchar(10) DEFAULT NULL,
   `pais` varchar(100) NOT NULL,
   `bairro` varchar(100) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `endereco_escola_empresa`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `empresa_id` INT UNSIGNED ,
-    `escola_id` INT UNSIGNED ,
-    `endereco` VARCHAR(255) NOT NULL,
-    `numero` INT NOT NULL,
-    `complemento` VARCHAR(255) NULL,
-    `cidade` VARCHAR(100) NOT NULL,
-    `estado` VARCHAR(100) NOT NULL,
-    `cep` VARCHAR(10) NOT NULL,
-    `pais` VARCHAR(100) NOT NULL,
-    FOREIGN KEY (`empresa_id`) REFERENCES `empresa`(`id`),
-    FOREIGN KEY (`escola_id`) REFERENCES `escola`(`id`)
-);
-
-CREATE TABLE `vaga`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `empresa_id` INT UNSIGNED NOT NULL,
-    `titulo` VARCHAR(255) NOT NULL,
-    `descricao` TEXT NOT NULL,
-    `requisitos` TEXT NOT NULL,
-    `data_publicacao` TIMESTAMP NOT NULL,
-    `data_encerramento` TIMESTAMP NOT NULL,
-    FOREIGN KEY (`empresa_id`) REFERENCES `empresa`(`id`)
-);
+-- --------------------------------------------------------
 
 --
--- Despejando dados para a tabela `estagiario`
+-- Estrutura para tabela `vaga`
 --
 
-INSERT INTO `estagiario` (`id`, `email`, `senha`, `nome`, `sobrenome`, `estado_civil`, `cpf`, `rg`, `rg_org_emissor`, `data_nascimento`, `telefone`, `celular`, `data_criacao`, `ultimo_login`, `status`, `rg_estado_emissor`, `nacionalidade`, `dependentes`, `cnh`, `genero`, `nome_social`, `endereco`, `numero`, `complemento`, `cidade`, `estado`, `cep`, `pais`, `bairro`) VALUES
-(1, 'carlos.d.verdeiro@gmail.com', '123', 'Carlos Daniel', 'Verdeiro', 'solteiro', '01234567890', '143873722', '', '2007-02-09', NULL, '44991567723', '2024-06-26 01:33:33', NULL, 1, 'SP', 'brasileira', 1, 'AB', 'M', 'oi', '', 0, NULL, '', '', '', '', NULL);
+CREATE TABLE `vaga` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `empresa_id` int(10) UNSIGNED NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `requisitos` text NOT NULL,
+  `data_publicacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `data_encerramento` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `empresa_email_unique` (`email`),
+  ADD UNIQUE KEY `empresa_cnpj_unique` (`cnpj`);
+
+--
+-- Índices de tabela `escola`
+--
+ALTER TABLE `escola`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `empresa_email_unique` (`email`),
+  ADD UNIQUE KEY `empresa_cnpj_unique` (`cnpj`);
 
 --
 -- Índices de tabela `estagiario`
@@ -102,16 +173,51 @@ ALTER TABLE `estagiario`
   ADD UNIQUE KEY `estagiario_rg_unique` (`rg`);
 
 --
+-- Índices de tabela `vaga`
+--
+ALTER TABLE `vaga`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `empresa_id` (`empresa_id`);
+
+--
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `escola`
+--
+ALTER TABLE `escola`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `estagiario`
 --
 ALTER TABLE `estagiario`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT de tabela `vaga`
+--
+ALTER TABLE `vaga`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `vaga`
+--
+ALTER TABLE `vaga`
+  ADD CONSTRAINT `vaga_ibfk_1` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
