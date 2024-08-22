b2a3076157e1eb158eb8aefeb5ae8d118a0ae1bd
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-08-22 14:38:45 -0300

    vaga

--- dashboard/pages/estagiario/vagas.php
+++ dashboard/pages/estagiario/vagas.php
@@ -45,72 +45,26 @@
                 </nav>
             </div>
         </div>
-            <div class="col m-1">
-                <div class="container-sm bg-light p-3 rounded">
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item">
-                            <a class="nav-link active " aria-current="page" href="#">Active</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Separated link</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <div class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">List group item heading</h5>
-                                <small>3 days ago</small>
+        <div class="card px-0" style="width: 18rem;">
+                                <div class="card-header">
+                                    <h5 class="card-title m-0">${vaga.titulo}</h5>
+                                </div>
+                                <div class="card-body">
+                                    <h6>Descrição:</h6>
+                                    <p class="card-text">${vaga.descricao}</p>
+                                    <h6>Requisitos:</h6>
+                                    <p class="card-text">${vaga.requisitos}</p>
+                                    <h6>Encerra:</h6>
+                                    <p class="card-text">${dataEncerramento}</p>
+                                    <h6>Publicado:</h6>
+                                    <p class="card-text">${formatarData(vaga.data_publicacao)}</p>
+                                </div>
+                                <div class="card-footer">
+                                    <button type="button" class="btn btn-primary sm btnVizualizar" value="${index}">Vizualizar</button>
+                                    <button type="button" class="btn btn-warning sm btnEditar" value="${index}">Editar</button>
+                                    <button type="button" class="btn btn-danger sm btnEncerrar" value="${index}" data-bs-toggle="modal" data-bs-target="#modalEncerrar">Encerrar</button>
+                                </div>
                             </div>
-                            <p class="mb-1">Some placeholder content in a paragraph.</p>
-                            <small>And some small print.</small>
-                        </a>
-                        <a href="#" class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">List group item heading</h5>
-                                <small class="text-body-secondary">3 days ago</small>
-                            </div>
-                            <p class="mb-1">Some placeholder content in a paragraph.</p>
-                            <small class="text-body-secondary">And some muted small print.</small>
-                        </a>
-                        <a href="#" class="list-group-item list-group-item-action">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">List group item heading</h5>
-                                <small class="text-body-secondary">3 days ago</small>
-                            </div>
-                            <p class="mb-1">Some placeholder content in a paragraph.</p>
-                            <small class="text-body-secondary">And some muted small print.</small>
-                        </a>
-                    </div>
-                    <nav aria-label="Page navigation example" class="mt-3 mb-0">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link">Voltar</a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Avançar</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
     </div>
 
 
