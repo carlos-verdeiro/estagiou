5519a8a310962b14c412d6ecedf72f45d693a7a8
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-09-12 14:30:16 -0300

    ajustes

--- dashboard/pages/estagiario/curriculo.php
+++ dashboard/pages/estagiario/curriculo.php
@@ -47,51 +47,6 @@ try {
         </div>
     </div>
 
-    <div class="divBlocos row row-cols-2">
-
-        <div class="blocos col-md arquivo visually-hidden mt-2">
-            <iframe id="iframeArquivo" frameborder="0" class="rounded"></iframe>
-        </div>
-
-        <div class="col-md mt-2 w-100 ">
-            <div class="row formulario visually-hidden bg-light rounded" id="divInformacoes">
-                <div class="p-3" enctype="multipart/form-data">
-                    <h4 class="form-label">Informações:</h4>
-                    <div class="mb-1">
-                        <div class="row">
-                            <h6 class="form-label col">Nome do arquivo:</h6>
-                            <p id="resNomeArquivo" class="col"></p>
-                        </div>
-                        <div class="row">
-                            <h6 class="form-label col">Data de submissão:</h6>
-                            <p id="resSubmissao" class="col"></p>
-                        </div>
-                        <div class="row">
-                            <h6 class="form-label col">Observações:</h6>
-                            <p id="resObservacoes"></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mt-2 row formulario w-100 bg-light rounded">
-                <form method="post" id="formUploadArquivo" class="p-3" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="curriculo" class="form-label">Faça upload de seu currículo aqui:</label>
-                        <input class="form-control form-control" id="curriculo" name="curriculo" type="file" accept="application/pdf" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="observacoes" class="form-label">Observações:</label>
-                        <textarea class="form-control form-control-sm" id="observacoes" name="observacoes" type="text" maxlength="500"></textarea>
-                    </div>
-                    <input type="submit" class="btn btn-primary sm" value="Salvar">
-                    <button type="button" class="btn btn-danger sm" id="btnExcluir" data-bs-toggle="modal" data-bs-target="#modalExcluir">Excluir</button>
-                </form>
-            </div>
-        </div>
-
-    </div>
-
     <div class="w-75  rounded mt-4">
         <div class="accordion" id="acordeao">
             <!-- Formação -->
@@ -306,6 +261,52 @@ try {
             </div>
         </div>
     </div>
+
+    <div class="divBlocos row row-cols-2">
+
+        <div class="blocos col-md arquivo visually-hidden mt-2">
+            <iframe id="iframeArquivo" frameborder="0" class="rounded"></iframe>
+        </div>
+
+        <div class="col-md mt-2 w-100 ">
+            <div class="row formulario visually-hidden bg-light rounded" id="divInformacoes">
+                <div class="p-3" enctype="multipart/form-data">
+                    <h4 class="form-label">Informações:</h4>
+                    <div class="mb-1">
+                        <div class="row">
+                            <h6 class="form-label col">Nome do arquivo:</h6>
+                            <p id="resNomeArquivo" class="col"></p>
+                        </div>
+                        <div class="row">
+                            <h6 class="form-label col">Data de submissão:</h6>
+                            <p id="resSubmissao" class="col"></p>
+                        </div>
+                        <div class="row">
+                            <h6 class="form-label col">Observações:</h6>
+                            <p id="resObservacoes"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-2 row formulario w-100 bg-light rounded">
+                <form method="post" id="formUploadArquivo" class="p-3" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="curriculo" class="form-label">Faça upload de seu currículo aqui:</label>
+                        <input class="form-control form-control" id="curriculo" name="curriculo" type="file" accept="application/pdf" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="observacoes" class="form-label">Observações:</label>
+                        <textarea class="form-control form-control-sm" id="observacoes" name="observacoes" type="text" maxlength="500"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-primary sm" value="Salvar">
+                    <button type="button" class="btn btn-danger sm" id="btnExcluir" data-bs-toggle="modal" data-bs-target="#modalExcluir">Excluir</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
 
     <!-- Modal Excluir -->
     <div class="modal fade" id="modalExcluir" tabindex="-1" aria-labelledby="modalExcluir" aria-hidden="true">
