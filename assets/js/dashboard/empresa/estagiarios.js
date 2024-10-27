d9a595a8cf58e416fb8bb77fcd755e98f4f7b689
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-10-27 14:45:25 -0300

    contratos

--- assets/js/dashboard/empresa/estagiarios.js
+++ assets/js/dashboard/empresa/estagiarios.js
@@ -1,62 +1,50 @@
 $(document).ready(function () {
     const main = $('#principal');
-
     const toastInformacao = new bootstrap.Toast($('#toastInformacao')[0]);
     const corpoToastInformacao = $('#corpoToastInformacao');
 
+    function formatarData(data) {
+        const [dataPart, horaPart] = data.split(' ');
+        const [ano, mes, dia] = dataPart.split('-');
+        return `${dia}/${mes}/${ano}`;
+    }
 
     function puxarEstagiarios() {
         $.getJSON('../../server/api/estagiarios/mostrarEstagiarios.php/estagiarios')
             .done(function (data) {
+                console.log(data.contratos);
                 main.empty();
-                if (data.length === 0) {
+                if (data.contratos.length === 0) {
                     main.append('<h3 class="text-center">Não há estagiários contratados</h3>');
                 } else {
                     main.append(`
-                    <div class="row mb-4">
-                        <div class="col-md-8">
+                        <div class="row mb-4">
+                            <div class="col-md-8">
                                 <input type="text" class="form-control" id="searchInput" placeholder="Buscar estagiário por nome...">
                             </div>
                             <div class="col-md-4 text-right">
                                 <button class="btn btn-primary">Filtrar</button>
                             </div>
                         </div>
-                
-                        <table class="table table-striped table-bordered">
-                            <thead class="thead-dark ">
-                                <tr>
-                                    <th>Nome</th>
-                                    <th>Vaga</th>
-                                    <th>Data de Contratação</th>
-                                    <th>Contato</th>
-                                </tr>
-                            </thead>
-                            <tbody id="estagiariosTable">
+                        <div id="estagiariosContainer" class="row"></div>
                     `);
-                    data.forEach((estagiario, index) => {
-                        blocosVagas.append(`
-                        <tr>
-                            <td>${data.nome}</td>
-                            <td>${data.vaga}</td>
-                            <td>${data.data_contratacao}</td>
-                            <td>${data.email}</td>
-                        </tr>
-                    `);
+
+                    const estagiariosContainer = $('#estagiariosContainer');
+                    data.contratos.forEach(estagiario => {
+                        estagiariosContainer.append(`
+                            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                                <div class="card h-100">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${estagiario.nome_estagiario} ${estagiario.sobrenome_estagiario}</h5>
+                                        <p class="card-text"><strong>Vaga:</strong> ${estagiario.titulo_vaga}</p>
+                                        <p class="card-text"><strong>Data de Contratação:</strong> ${formatarData(estagiario.data_contratacao)}</p>
+                                        <p class="card-text"><strong>Contato:</strong> ${estagiario.email_estagiario}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        `);
                     });
                 }
-                main.append(`
-                        </tbody>
-                </table>
-                <nav aria-label="Page navigation">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
-                </ul>
-            </nav>
-                `);
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 corpoToastInformacao.text(`Erro ao obter os dados: ${textStatus}`);
@@ -64,9 +52,6 @@ $(document).ready(function () {
                 console.error(errorThrown);
             });
     }
-
-
-
 
     puxarEstagiarios();
 });
---
+++ server/api/estagiarios/mostrarEstagiarios.php
@@ -0,0 +1,122 @@
+<?php
+
+session_start();
+header('Content-Type: application/json');
+// Verificação de autenticação e tipo de usuário
+if (
+    !isset($_SESSION['statusLogin'], $_SESSION['tipoUsuarioLogin']) ||
+    $_SESSION['statusLogin'] !== 'autenticado' ||
+    $_SESSION['tipoUsuarioLogin'] !== 'empresa'
+) {
+    http_response_code(401); // Não autorizado
+    echo json_encode(['mensagem' => 'Usuário não autenticado.', 'code' => 2]);
+    exit;
+}
+
+// Verificação do método de requisição
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405); // Método não permitido
+    echo json_encode(['mensagem' => 'Método de requisição inválido.', 'code' => 1]);
+    exit;
+}
+
+// Sanitização e processamento da URI
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+
+$idEmpresa = $_SESSION['idUsuarioLogin'];
+
+$busca = isset($uri[5]) ? $uri[5] : null;
+switch ($busca) {
+    case 'estagiarios':
+        // -----PAGINAÇÃO
+        $partida = isset($uri[6]) && is_numeric($uri[6]) ? (int)$uri[6] : 0; //inicio
+        $limiteBusca = isset($uri[7]) && is_numeric($uri[7]) ? (int)$uri[7] : 50; //limite
+
+        try {
+            include_once '../../conexao.php';
+
+            // Consulta para buscar as vagas e verificar candidaturas
+            $stmt = $conn->prepare("
+            SELECT c.id AS id_contrato,
+                c.data_contratacao,
+                e.id AS id_estagiario,
+                e.nome AS nome_estagiario,
+                e.sobrenome AS sobrenome_estagiario,
+                e.email AS email_estagiario,
+                e.celular AS celular_estagiario,
+                v.titulo AS titulo_vaga,
+                v.descricao AS descricao_vaga
+            FROM contratos c
+            INNER JOIN estagiario e ON c.id_estagiario = e.id
+            INNER JOIN vaga v ON c.id_vaga = v.id
+            WHERE c.id_empresa = ?
+            ORDER BY c.data_contratacao ASC
+            LIMIT ?
+            OFFSET ?;
+
+            ");
+
+            $statusVaga = -1;
+
+            $stmt->bind_param("iii", $idEmpresa, $limiteBusca, $partida);
+            if (!$stmt->execute()) {
+                throw new Exception("Erro ao executar a consulta: " . $stmt->error);
+            }
+
+            $result = $stmt->get_result();
+            $contratos = [];
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $contratos[] = $row;
+                }
+            }
+
+            // Consulta para contar o total de registros, incluindo os mesmos filtros
+            $stmt_total = $conn->prepare("
+            SELECT COUNT(*) AS total_registros
+            FROM contratos
+            WHERE contratos.id_empresa = ?
+            ");
+
+            if (!$stmt_total) {
+                throw new Exception("Erro na preparação da consulta de contagem: " . $conn->error);
+            }
+
+            $stmt_total->bind_param("i", $idEmpresa);
+
+            if (!$stmt_total->execute()) {
+                throw new Exception("Erro ao executar a consulta de contagem: " . $stmt_total->error);
+            }
+
+            $total_result = $stmt_total->get_result();
+            $total_registros = $total_result->fetch_assoc()['total_registros'];
+
+            // Converte o array em JSON, incluindo o total de registros
+            $json_data = json_encode([
+                'total_registros' => $total_registros,
+                'contratos' => $contratos
+            ]);
+
+            echo $json_data;
+
+            // Fecha as consultas
+            $stmt->close();
+            $stmt_total->close();
+        } catch (Exception $e) {
+            http_response_code(500); // Erro interno do servidor
+            error_log('Erro interno: ' . $e->getMessage()); // Loga o erro para análise
+            echo json_encode(['mensagem' => 'Erro interno do servidor.' . $e->getMessage(), 'code' => 3]);
+        } finally {
+            if (isset($conn)) $conn->close();
+        }
+
+        break;
+
+
+    default:
+        echo json_encode(['mensagem' => 'Parâmetros incorretos.', 'code' => 1]);
+        exit;
+        break;
+}
